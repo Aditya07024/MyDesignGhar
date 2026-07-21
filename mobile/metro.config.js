231dfc01cecb6eb99b860005a4dd1037d046b0af
Aditya07024 <adityakumar07024@gmail.com>
2026-07-21 16:07:18 +0530

    fix: add env variables to eas.json for EAS cloud builds (API URL + Clerk key)

--- mobile/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-// Enable package exports resolution to correctly handle modern package ESM structures
-config.resolver.unstable_enablePackageExports = false;
-
-module.exports = config;
